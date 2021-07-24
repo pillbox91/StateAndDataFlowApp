69d4d2045288b5d43e55ecd8f23016af682d56b1
Andrey Averyanov <averyanov1996@gmail.com>
2021-07-24 20:41:49 +0300

    Initial Commit

---
+++ StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by Андрей Аверьянов on 24.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
